5d67bcecffa6e9120ad3b48659df52c29f059fbf
CometOnOrbit <cometonorbit@163.com>
2025-01-26 17:05:19 +0800

    Update

--- cmake/FindMySQL.cmake
+++ cmake/FindMySQL.cmake
@@ -1,25 +1,18 @@
+set(MYSQL_CPPCONN_LIBRARY)
+set(Boost_INCLUDE_DIRS)
+
 if(NOT CMAKE_CROSSCOMPILING)
   find_program(MYSQL_CONFIG
     NAMES mysql_config mariadb_config
   )
 
   if(MYSQL_CONFIG)
-    exec_program(${MYSQL_CONFIG}
-      ARGS --include
-      OUTPUT_VARIABLE MY_TMP
-    )
-
+    execute_process(COMMAND ${MYSQL_CONFIG} --include OUTPUT_VARIABLE MY_TMP)
     string(REGEX REPLACE "-I([^ ]*)( .*)?" "\\1" MY_TMP "${MY_TMP}")
-
     set(MYSQL_CONFIG_INCLUDE_DIR ${MY_TMP} CACHE FILEPATH INTERNAL)
 
-    exec_program(${MYSQL_CONFIG}
-      ARGS --libs_r
-      OUTPUT_VARIABLE MY_TMP
-    )
-
+    execute_process(COMMAND ${MYSQL_CONFIG} --libs_r OUTPUT_VARIABLE MY_TMP)
     set(MYSQL_CONFIG_LIBRARIES "")
-
     string(REGEX MATCHALL "-l[^ ]*" MYSQL_LIB_LIST "${MY_TMP}")
     foreach(LIB ${MYSQL_LIB_LIST})
       string(REGEX REPLACE "[ ]*-l([^ ]*)" "\\1" LIB "${LIB}")
@@ -27,7 +20,6 @@ if(NOT CMAKE_CROSSCOMPILING)
     endforeach()
 
     set(MYSQL_CONFIG_LIBRARY_PATH "")
-
     string(REGEX MATCHALL "-L[^ ]*" MYSQL_LIBDIR_LIST "${MY_TMP}")
     foreach(LIB ${MYSQL_LIBDIR_LIST})
       string(REGEX REPLACE "[ ]*-L([^ ]*)" "\\1" LIB "${LIB}")
@@ -38,47 +30,48 @@ endif()
 
 set_extra_dirs_lib(MYSQL mysql)
 find_library(MYSQL_LIBRARY
-  NAMES "mysqlclient" "mysqlclient_r" "mariadbclient"
-  HINTS ${HINTS_MYSQL_LIBDIR} ${MYSQL_CONFIG_LIBRARY_PATH}
-  PATHS ${PATHS_MYSQL_LIBDIR}
+  NAMES "mysqlcppconn" "mysqlcppconn-static"
+  HINTS ${MYSQL_CONFIG_LIBRARY_PATH}
   ${CROSSCOMPILING_NO_CMAKE_SYSTEM_PATH}
 )
 set_extra_dirs_include(MYSQL mysql "${MYSQL_LIBRARY}")
 find_path(MYSQL_INCLUDEDIR
-  NAMES "mysql.h"
-  HINTS ${HINTS_MYSQL_INCLUDEDIR} ${MYSQL_CONFIG_INCLUDE_DIR}
-  PATHS ${PATHS_MYSQL_INCLUDEDIR}
-  ${CROSSCOMPILING_NO_CMAKE_SYSTEM_PATH}
-)
-
-set_extra_dirs_lib(MYSQL_CPPCONN mysql)
-find_library(MYSQL_CPPCONN_LIBRARY
-  NAMES "mysqlcppconn" "mysqlcppconn-static"
-  HINTS ${HINTS_MYSQL_CPPCONN_LIBDIR} ${MYSQL_CONFIG_LIBRARY_PATH}
-  PATHS ${PATHS_MYSQL_CPPCONN_LIBDIR}
-  ${CROSSCOMPILING_NO_CMAKE_SYSTEM_PATH}
-)
-set_extra_dirs_include(MYSQL_CPPCONN mysql "${MYSQL_CPPCONN_LIBRARY}")
-find_path(MYSQL_CPPCONN_INCLUDEDIR
   NAMES "mysql_connection.h"
-  HINTS ${HINTS_MYSQL_CPPCONN_INCLUDEDIR} ${MYSQL_CONFIG_INCLUDE_DIR}
-  PATHS ${PATHS_MYSQL_CPPCONN_INCLUDEDIR}
+  HINTS ${MYSQL_CONFIG_INCLUDE_DIR}
   ${CROSSCOMPILING_NO_CMAKE_SYSTEM_PATH}
 )
 
 include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(MySQL DEFAULT_MSG MYSQL_LIBRARY MYSQL_INCLUDEDIR)
+find_package_handle_standard_args(MySQL DEFAULT_MSG MYSQL_LIBRARY MYSQL_INCLUDEDIR) 
+set(Boost_INCLUDE_DIRS "${PROJECT_SOURCE_DIR}/libraries/boost/include")
 
-if(MYSQL_FOUND)
-  is_bundled(MYSQL_BUNDLED "${MYSQL_LIBRARY}")
+if(NOT(MYSQL_FOUND))
+  find_library(MYSQL_LIBRARY
+    NAMES "mysqlcppconn" "mysqlcppconn-static"
+    HINTS ${HINTS_MYSQL_LIBDIR} ${MYSQL_CONFIG_LIBRARY_PATH}
+    PATHS ${PATHS_MYSQL_LIBDIR}
+    ${CROSSCOMPILING_NO_CMAKE_SYSTEM_PATH}
+  )
+  set_extra_dirs_include(MYSQL mysql "${MYSQL_LIBRARY}")
+  find_path(MYSQL_INCLUDEDIR
+    NAMES "mysql_connection.h"
+    HINTS ${HINTS_MYSQL_INCLUDEDIR} ${MYSQL_CONFIG_INCLUDE_DIR}
+    PATHS ${PATHS_MYSQL_INCLUDEDIR}
+    ${CROSSCOMPILING_NO_CMAKE_SYSTEM_PATH}
+  )
 
-  set(MYSQL_LIBRARIES ${MYSQL_LIBRARY} ${MYSQL_CPPCONN_LIBRARY})
-  set(MYSQL_INCLUDE_DIRS ${MYSQL_INCLUDEDIR} ${MYSQL_CPPCONN_INCLUDEDIR})
-  
-  set(MYSQL_COPY_FILES)
-  if(TARGET_OS AND TARGET_OS STREQUAL "windows")
-   set(MYSQL_COPY_FILES "${EXTRA_MYSQL_LIBDIR}/mysqlcppconn-7-vs14.dll" "${EXTRA_MYSQL_LIBDIR}/libeay32.dll" "${EXTRA_MYSQL_LIBDIR}/ssleay32.dll")
+  find_package_handle_standard_args(MySQL DEFAULT_MSG MYSQL_LIBRARY MYSQL_INCLUDEDIR)
+
+  if(MYSQL_FOUND)
+    if(TARGET_OS AND TARGET_OS STREQUAL "windows")
+      set(MYSQL_COPY_FILES "${EXTRA_MYSQL_LIBDIR}/mysqlcppconn-7-vs14.dll")
+    else()
+      set(MYSQL_COPY_FILES)
+    endif()
   endif()
-  
-  mark_as_advanced(MYSQL_INCLUDEDIR MYSQL_LIBRARY)
 endif()
+
+set(MYSQL_LIBRARIES ${MYSQL_LIBRARY})
+set(MYSQL_INCLUDE_DIRS ${MYSQL_INCLUDEDIR} ${Boost_INCLUDE_DIRS})
+
+mark_as_advanced(MYSQL_INCLUDEDIR MYSQL_LIBRARY)
